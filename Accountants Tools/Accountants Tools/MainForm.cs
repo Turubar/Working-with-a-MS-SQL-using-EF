ff78ba0bf7a37a13e1b74e551f41fd3e43a5fd62
Turubar <ivan.turubar@gmail.com>
2022-12-05 15:29:12 +0300

    generated entity context

--- Accountants Tools/Accountants Tools/MainForm.cs
+++ Accountants Tools/Accountants Tools/MainForm.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Accountants_Tools.entity_framework;
 
 namespace Accountants_Tools
 {
